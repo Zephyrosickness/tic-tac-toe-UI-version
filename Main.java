21694fd6bce4f15d4537fac9b5425ada9cd25de8
Zephyros <zxphyros@gmail.com>
2024-11-19 18:20:45 -0500

    make basic cell coloring logic

--- Main.java
+++ Main.java
@@ -1,43 +1,56 @@
-import java.util.Arrays;
+import java.awt.*;
 import java.util.Objects;
 import javax.swing.*;
 
 public class Main {
     //init var
     private static final int BOARD_SIZE = 3;
-    private static final String[][] board = new String[BOARD_SIZE][BOARD_SIZE];
+    private static final Cell[][] board = new Cell[BOARD_SIZE][BOARD_SIZE];
     private static final String EMPTY_BOX = "-";
     private static boolean isWon = false;
+    private static Player currentTurn;
 
+    //inits all important stuff
     public static void main(String[] args){
+        //inits players
+        boolean isPlaying = true;
+        final Player player1 = new Player("Player 1", Color.RED);
+        final Player player2 = new Player("Player 2", Color.BLUE);
+        Player[] players = new Player[]{player1, player2};
+        currentTurn = player1;
+
+        //ui management inits
         final int WINDOW_SIZE = 500;
+        final int DISPLACEMENT = 15;
+
         JFrame frame = new JFrame("tic tac toe");
-        frame.setSize(WINDOW_SIZE,WINDOW_SIZE);
+        frame.setSize(WINDOW_SIZE,WINDOW_SIZE+WINDOW_SIZE/6);
         frame.setVisible(true);
 
-        boolean isPlaying = true;
-        final Player player1 = new Player("Player 1","X");
-        final Player player2 = new Player("Player 2","O");
-        Player[] players = new Player[]{player1, player2};
-        boolean namePlayerLoop;
-        clearBoard();
+        //the boxes go here
+        JPanel cellPanel = new JPanel();
+        cellPanel.setLayout(new GridLayout(BOARD_SIZE, BOARD_SIZE));
+        cellPanel.setBounds(DISPLACEMENT, DISPLACEMENT, WINDOW_SIZE-DISPLACEMENT*2, WINDOW_SIZE-DISPLACEMENT);
+        frame.add(cellPanel);
 
-        //changing settings upon initialization
-        /*if(InputHelper.getYN("Welcome to tic-tac toe!\nBefore we begin, do you want to set any names for players?")){
-            do {
-                Player selectedPlayer = players[InputHelper.getRangedInt("Select a Player to change.", 1, 2)-1]; //has to be subtracted by 1 because the indexes start at 0
-                selectedPlayer.name = InputHelper.getString("What do you want to change this player's name to?");
-                System.out.println("Player name set to "+selectedPlayer.name+".");
-                namePlayerLoop = InputHelper.getYN("Do you want to name another player?");
-            }while(namePlayerLoop);
-        }*/
+        //the text (such as "player X's turn!") goes here
+        JPanel consolePanel = new JPanel();
+        consolePanel.setLayout(null);
+        consolePanel.setBounds(WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE/6);
+        frame.add(consolePanel);
 
-        displayBoard();
+        //add logic to change player names here
+
+        //inits all cells into board
+        for(int row = 0;row<BOARD_SIZE;row++){
+            for(int col = 0; col <BOARD_SIZE; col++){
+                new Cell(cellPanel, row,col);
+            }
+        }
 
         //main gameplay loop
         do{
-            setCell(players[0]); //player 1's move
-            setCell(players[1]); //player 2's move
+
 
             //if win, do a YN input for the isplaying bool
             if(isWon){
@@ -45,14 +58,6 @@ public class Main {
             }
 
         }while(isPlaying);
-    }
-
-    private static void clearBoard(){for(int i = 0; i<BOARD_SIZE; i++){board[i] = new String[]{EMPTY_BOX, EMPTY_BOX, EMPTY_BOX};}}
-
-    private static void displayBoard(){
-        System.out.println();
-        for(String[] i:board){System.out.println(Arrays.toString(i));}
-        System.out.println();
     }
 
     //this is a boolean bc u need a way to tell the main method the game is over so they can prompt to play again
@@ -64,15 +69,14 @@ public class Main {
 
         //prevents game from still playing if the player already won
         if(!isWon){
-            do {
+            //do {
                 //row = InputHelper.getRangedInt(player.name + ", enter move row",1,3);
                 //col = InputHelper.getRangedInt(player.name + ", enter move column",1,3);
                 //legal = legalMoveCheck(row, col);
-                if (!legal) {System.out.println("Sorry, you entered invalid options. Try again.");}
-            }while(!legal);
+                //if (!legal) {System.out.println("Sorry, you entered invalid options. Try again.");}
+            //}while(!legal);
 
-            board[row-1][col-1] = player.icon; //must be appended by 1 because the indexes are 0-2
-            displayBoard();
+            //board[row-1][col-1] = player.icon; //must be appended by 1 because the indexes are 0-2
             isWon = winCheck(player);
         }
     }
@@ -102,7 +106,7 @@ public class Main {
 
     //checks for horizontal win
     private static boolean horizontalWinCheck(Player player){
-        for(String[] i:board){if (Arrays.equals(i, new String[]{player.icon, player.icon, player.icon})){return true;}}
+        //for(Cell[] i:board){if (Arrays.equals(i, new String[]{player.icon, player.icon, player.icon})){return true;}}
         return false;
     }
 
@@ -110,8 +114,8 @@ public class Main {
     private static boolean verticalWinCheck(Player player){
         for(int col = 0; col<BOARD_SIZE; col++){
             boolean win = true;
-            for(String[] row : board){
-                if(!row[col].equals(player.icon)){
+            for(Cell[] row : board){
+                if(!row[col].field.getBackground().equals(player.color)){
                     win = false;
                     break;
                 }
@@ -126,14 +130,14 @@ public class Main {
         boolean rightToLeft = true;
         //checks if diagonal from left to right
         for(int i = 0; i<BOARD_SIZE;i++){
-            if(!board[i][i].equals(player.icon)){
+            if(!board[i][i].field.getBackground().equals(player.color)){
                 leftToRight = false;
                 break;
             }
         }
         //checks if diagonal from right to left
         for(int i = BOARD_SIZE-1; i>0;i--){
-            if(!board[i][(BOARD_SIZE-1)-i].equals(player.icon)){
+            if(!board[i][(BOARD_SIZE-1)-i].field.getBackground().equals(player.color)){
                 rightToLeft = false;
                 break;
             }
@@ -144,9 +148,9 @@ public class Main {
     private static boolean tieCheck(){
         boolean isTied = true;
 
-        for(String[] row:board){
-            for(String col:row){
-                if(col.equals(EMPTY_BOX)){
+        for(Cell[] row:board){
+            for(Cell col:row){
+                if(col.taken){
                     isTied = false;
                     break;
                 }
@@ -157,11 +161,34 @@ public class Main {
 
     private static class Player{
         String name;
-        String icon; // X or O
+        Color color;
 
-        private Player(String name, String icon){
+        private Player(String name, Color color){
             this.name = name;
-            this.icon = icon;
+            this.color = color;
+        }
+    }
+    private static class Cell{
+        boolean taken;
+        JButton field = new JButton();
+
+        private Cell(JPanel panel, int row, int col){
+            taken = false;
+
+            field.setBackground(Color.WHITE);
+            panel.add(field);
+
+            board[row][col] = this;
+
+            field.addActionListener(e -> {
+                setBackground(currentTurn);
+                turnAdvancement();
+            });
+        }
+
+        private void setBackground(Player player){
+            taken = true;
+            field.setBackground(player.color);
         }
     }
 }
