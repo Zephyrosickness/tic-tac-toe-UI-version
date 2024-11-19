65103ba40bd868caad220cb9c17ccb2639d349f6
Zephyros <zxphyros@gmail.com>
2024-11-19 18:56:40 -0500

    made the color actually work + label

--- Main.java
+++ Main.java
@@ -5,26 +5,27 @@ import javax.swing.*;
 public class Main {
     //init var
     private static final int BOARD_SIZE = 3;
-    private static final Cell[][] board = new Cell[BOARD_SIZE][BOARD_SIZE];
-    private static final String EMPTY_BOX = "-";
-    private static boolean isWon = false;
+    private static final Cell[] board = new Cell[BOARD_SIZE][BOARD_SIZE];
     private static Player currentTurn;
+    private static Player[] players;
+    private static JLabel currentTurnLabel;
 
     //inits all important stuff
     public static void main(String[] args){
         //inits players
         boolean isPlaying = true;
-        final Player player1 = new Player("Player 1", Color.RED);
-        final Player player2 = new Player("Player 2", Color.BLUE);
-        Player[] players = new Player[]{player1, player2};
+        final Player player1 = new Player("Player 1", Color.RED, 0);
+        final Player player2 = new Player("Player 2", Color.BLUE, 1);
+        players = new Player[]{player1, player2};
         currentTurn = player1;
+        currentTurn.id = 0;
 
         //ui management inits
         final int WINDOW_SIZE = 500;
         final int DISPLACEMENT = 15;
 
         JFrame frame = new JFrame("tic tac toe");
-        frame.setSize(WINDOW_SIZE,WINDOW_SIZE+WINDOW_SIZE/6);
+        frame.setSize(WINDOW_SIZE,WINDOW_SIZE+(int)((double)WINDOW_SIZE/3));
         frame.setVisible(true);
 
         //the boxes go here
@@ -35,73 +36,22 @@ public class Main {
 
         //the text (such as "player X's turn!") goes here
         JPanel consolePanel = new JPanel();
+        consolePanel.setBounds(0, WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE/6);
         consolePanel.setLayout(null);
-        consolePanel.setBounds(WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE/6);
         frame.add(consolePanel);
+
+        //add label to display current player's turn here
+        currentTurnLabel = new JLabel(currentTurn.name+"'s turn!");
+        currentTurnLabel.setBounds((int) (WINDOW_SIZE/2.75), WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE/6);
+        consolePanel.add(currentTurnLabel);
 
         //add logic to change player names here
 
         //inits all cells into board
-        for(int row = 0;row<BOARD_SIZE;row++){
-            for(int col = 0; col <BOARD_SIZE; col++){
-                new Cell(cellPanel, row,col);
-            }
-        }
-
-        //main gameplay loop
-        do{
-
-
-            //if win, do a YN input for the isplaying bool
-            if(isWon){
-                //isPlaying = InputHelper.getYN("Would you like to play again?");
-            }
-
-        }while(isPlaying);
-    }
-
-    //this is a boolean bc u need a way to tell the main method the game is over so they can prompt to play again
-    private static void setCell(Player player){
-        //tbh i could just use a regex to ensure the player inputs something along the lines of [X, Y] and then read the R/C using substrings and integer.parseInt but tbh ive already overengineered this program enough
-        boolean legal;
-        int row ;
-        int col;
-
-        //prevents game from still playing if the player already won
-        if(!isWon){
-            //do {
-                //row = InputHelper.getRangedInt(player.name + ", enter move row",1,3);
-                //col = InputHelper.getRangedInt(player.name + ", enter move column",1,3);
-                //legal = legalMoveCheck(row, col);
-                //if (!legal) {System.out.println("Sorry, you entered invalid options. Try again.");}
-            //}while(!legal);
-
-            //board[row-1][col-1] = player.icon; //must be appended by 1 because the indexes are 0-2
-            isWon = winCheck(player);
-        }
-    }
-
-    private static boolean legalMoveCheck(int row, int col){
-        boolean rowCheck = false;
-        boolean colCheck = false;
-        boolean alreadyExists = true;
-        if(row>=1&&row<=BOARD_SIZE){rowCheck=true;}
-        if(col>=1&&col<=BOARD_SIZE){colCheck=true;}
-        if(!Objects.equals(board[row - 1][col - 1], EMPTY_BOX)){alreadyExists = false;}
-        return rowCheck&&colCheck&&alreadyExists;
-    }
-
-    private static boolean winCheck(Player player){
-        boolean horizontal = horizontalWinCheck(player);
-        boolean vertical = verticalWinCheck(player);
-        boolean diagonal = diagonalWinCheck(player);
-        boolean playerWon = horizontal||vertical||diagonal;
-        boolean tie = tieCheck();
-
-        if(horizontal||vertical||diagonal){System.out.println(player.name+" won!");
-        }else if(tie){System.out.println("Tie!");}
-
-        return tie||playerWon;
+        int CELL_COUNT = (int) Math.pow(BOARD_SIZE, 2);
+        for(int pos = 0; pos < CELL_COUNT; pos++){new Cell(cellPanel, pos);}
+        cellPanel.repaint();
+        cellPanel.revalidate();
     }
 
     //checks for horizontal win
@@ -157,38 +107,51 @@ public class Main {
             }
         }
         return isTied;
-    }
+    }*/
 
     private static class Player{
         String name;
         Color color;
+        int id;
 
-        private Player(String name, Color color){
+        private Player(String name, Color color, int id){
             this.name = name;
             this.color = color;
+            this.id = id;
         }
     }
     private static class Cell{
         boolean taken;
         JButton field = new JButton();
+        int pos;
 
-        private Cell(JPanel panel, int row, int col){
+        private Cell(JPanel panel, int pos){
             taken = false;
 
             field.setBackground(Color.WHITE);
             panel.add(field);
+            this.pos = pos;
 
-            board[row][col] = this;
+            board[pos] = this;
 
-            field.addActionListener(e -> {
-                setBackground(currentTurn);
+            field.addActionListener(e -> {setBackground();});
+        }
+
+        private void setBackground(){
+            if(!taken) {
+                taken = true;
+                field.setBackground(currentTurn.color);
                 turnAdvancement();
-            });
+            }
         }
+    }
 
-        private void setBackground(Player player){
-            taken = true;
-            field.setBackground(player.color);
-        }
+    private static void turnAdvancement(){
+        int playerID = currentTurn.id+1;
+        if(playerID>=2){playerID = 0;}
+
+        currentTurn = players[playerID];
+
+        currentTurnLabel.setText(currentTurn.name+"'s turn!");
     }
 }
